1aa14cc5875f5f3d490ff53e9341b52fd6be32be
chloeehulme <89981993+chloeehulme@users.noreply.github.com>
2022-09-22 10:29:36 +1000

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,18 @@
+namespace robot_controller_api;
+
+public class Map
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Columns { get; set; }
+    public int Rows { get; set; }
+    public bool IsSquare { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string? Description { get; set; }
+
+    public Map()
+    {
+
+    }
+}
